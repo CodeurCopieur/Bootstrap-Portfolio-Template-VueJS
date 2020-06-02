ca608be876d27ac73de7f39ff85b0d762710df59
codwerk <louis_widdy@live.fr>
2020-06-02 14:12:06 +0200

    end html componont offer

---
+++ src/assets/components/offer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+    <div class="icon mx-auto">
+      <i class="fas" :class="offer.icon"></i>
+      <h4>{{ offer.title }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'offer',
+  props: ['offer']
+}
+</script>
+
+<style lang="scss">
+
+</style>
