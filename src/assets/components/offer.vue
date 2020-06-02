45e5ba33f0e7f1bde48725bea88120f6fed80402
codwerk <louis_widdy@live.fr>
2020-06-02 14:12:43 +0200

    end html componont offer v2

--- src/assets/components/offer.vue
+++ src/assets/components/offer.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+  <div id="card-offer" class="col-lg-3 col-md-6 mb-5 mb-lg-0">
     <div class="icon mx-auto">
       <i class="fas" :class="offer.icon"></i>
       <h4>{{ offer.title }}</h4>
+      <p>{{ offer.description }}</p>
     </div>
   </div>
 </template>
